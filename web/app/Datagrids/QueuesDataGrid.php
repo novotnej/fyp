5afe4f0ab55eb17f2da78c96d801b08012d7ffee
Jan Novotny <novotny@profisites.it>
2019-12-14 19:57:10 +0000

    WEB: Devices and Queues bare DataGrids

---
+++ web/app/Datagrids/QueuesDataGrid.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\DataGrids;
+
+use App\Model\Queue;
+
+/**
+ * Class DevicesDatagrid
+ * @package App\DataGrids
+ */
+class QueuesDataGrid extends CommonDataGrid {
+    protected function init() {
+        parent::init();
+        $this->setDataSource($this->orm->queues->findAll());
+        $this->addColumnNumber('id', 'ID')->setSortable();
+        $this->addColumnText('name', 'Name')->setSortable()->setFilterText();
+
+        //$this->addAction('edit', 'Edit', 'Device:edit')->setIcon('edit');
+        $this->setDefaultPerPage(50);
+    }
+}
--- web/app/FrontModule/Presenters/HomepagePresenter.php
+++ web/app/FrontModule/Presenters/HomepagePresenter.php
@@ -2,6 +2,9 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\DataGrids\DevicesDataGrid;
+use App\DataGrids\QueuesDataGrid;
+
 class HomepagePresenter extends BasePresenter {
 
     public function actionDefault() {
@@ -11,4 +14,19 @@ class HomepagePresenter extends BasePresenter {
     public function renderDefault() {
     }
 
+    /**
+     * @param $name
+     * @return DevicesDataGrid
+     */
+    protected function createComponentDevicesDataGrid($name) {
+        return $this->createDataGrid('devices', $name);
+    }
+
+    /**
+     * @param $name
+     * @return QueuesDataGrid
+     */
+    protected function createComponentQueuesDataGrid($name) {
+        return $this->createDataGrid("queues", $name);
+    }
 }
