9f761dae8957526ffadf3260d89744293da9465e
Jan Novotný <novotny@profisites.it>
2020-04-03 16:10:58 +0200

    Experiment - experiment scripts - add name argument

--- experiment/scripts/cached.sh
+++ experiment/scripts/cached.sh
@@ -1,46 +1,46 @@
-mpirun -np 10 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 10 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 100 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 100 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 200 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 200 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 300 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 300 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 400 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 400 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 500 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 500 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 600 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 600 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 700 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 700 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 800 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 800 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 900 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 900 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1000 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1000 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1100 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1100 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1200 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1200 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1300 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1300 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1400 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1400 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1500 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1500 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1600 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1600 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1700 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1700 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1800 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1800 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 1900 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 1900 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 2000 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 2000 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 2100 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 2100 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
-mpirun -np 2200 --hostfile hostfile ./a.out 20000 100 0
+mpirun -np 2200 --hostfile hostfile ./a.out 20000 100 0 cached
 sleep 2
