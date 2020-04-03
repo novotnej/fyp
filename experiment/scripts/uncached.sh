a115e72fe0e480cfa7ef97e5f6cdcd12205d7ed2
Jan Novotný <novotny@profisites.it>
2020-04-03 16:41:19 +0200

    Experiment - shorten experiment name to 4 characters

--- experiment/scripts/uncached.sh
+++ experiment/scripts/uncached.sh
@@ -1,40 +1,40 @@
-mpirun -np 1 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 1 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 2 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 2 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 3 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 3 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 4 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 4 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 5 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 5 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 6 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 6 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 7 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 7 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 8 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 8 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 9 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 9 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 10 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 10 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 11 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 11 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 12 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 12 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 13 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 13 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 14 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 14 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 15 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 15 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 16 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 16 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 17 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 17 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 18 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 18 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 19 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 19 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
-mpirun -np 20 --hostfile hostfile ./a.out 20000 100 0 uncached
+mpirun -np 20 --hostfile hostfile ./a.out 20000 100 0 unca
 sleep 2
