febd76cefb0467d81c661c0540a4c5481dce7317
Jan Novotný <novotny@profisites.it>
2020-04-05 22:11:09 +0200

    Experiment - cached reqs per second script

---
+++ experiment/scripts/cached_rps.sh
@@ -0,0 +1,40 @@
+mpirun -np 5 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 10 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 15 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 20 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 25 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 30 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 35 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 40 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 45 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 50 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 55 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 60 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 65 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 70 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 75 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 80 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 85 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 90 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 95 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
+mpirun -np 100 --hostfile hostfile ./a.out 20000 10000 0 carp
+sleep 2
