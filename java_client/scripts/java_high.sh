16b541c9be6be3996b767bbf414ef65b72ba6671
Jan Novotný <novotny@profisites.it>
2020-04-06 12:51:14 +0200

    Java client - high and low scripts

---
+++ java_client/scripts/java_high.sh
@@ -0,0 +1,46 @@
+mpirun -np 10 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 100 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 200 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 300 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 400 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 500 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 600 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 700 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 800 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 900 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1000 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1100 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1200 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1300 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1400 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1500 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1600 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1700 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1800 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 1900 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 2000 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 2100 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
+mpirun -np 2200 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000 
+sleep 2
---
+++ java_client/scripts/java_low.sh
@@ -0,0 +1,40 @@
+mpirun -np 1 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 2 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 3 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 4 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 5 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 6 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 7 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 8 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 9 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 10 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 11 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 12 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 13 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 14 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 15 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 16 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 17 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 18 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 19 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
+mpirun -np 20 --hostfile ../experiment/hostfile java -classpath "./lib/*" Client 1000000 100 10000000
+sleep 2
