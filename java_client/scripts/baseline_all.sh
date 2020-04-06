7d9fc39bbc5811d3b08b17326a3bd9e6cef6be9a
Jan Novotný <novotny@profisites.it>
2020-04-06 12:30:47 +0200

    Java client - baseline scripts

--- java_client/scripts/baseline_all.sh
+++ java_client/scripts/baseline_all.sh
@@ -1,6 +1,56 @@
-mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 100 100 10000
-mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1000 100 10000
-mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 10000 100 10000
 mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 100000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 200000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 300000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 400000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 500000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 600000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 700000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 800000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 900000 100 10000
+sleep 2
 mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1100000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1200000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1300000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1400000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1500000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1600000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1700000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1800000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1900000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 2000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 3000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 4000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 5000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 6000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 7000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 8000000 100 10000
+sleep 2
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 9000000 100 10000
+sleep 2
 mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 10000000 100 10000
+sleep 2
---
+++ java_client/scripts/baseline_proper.sh
@@ -0,0 +1,1 @@
+mpirun -np 1 -hostfile ../experiment//hostfile java -classpath "./lib/*" Client 1000000 1000 10000
